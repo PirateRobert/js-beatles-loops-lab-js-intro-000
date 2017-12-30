c63b8aa4175c7fb9c3ccd089ee492db36112a09b
This is Rude <dbauman1@swarthmore.edu>
2017-12-30 02:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function theBeatlesPlay(arrayM,arrayI){
   var newarray = {}
-  for (let i = 0;i<arrayM.length; i++)
+  for (let i = 0;i<arrayM.length; i++){
+    var musician = arrayM.slice(i,i+1)
+    var instrument = arrayI.slice(i,i+1)
+    newarray.push(`${musician} plays ${instrument}`)
+  }
+  return newarray
+}
+
+function johnLennonFacts(array){
+
 }
